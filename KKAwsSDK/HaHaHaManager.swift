0beefb9500280b90e1684ed9e00a0e505f69f9a6
Khaleel <khaleelk@yupptv.com>
2019-01-10 12:48:06 +0530

    Initial commit

---
+++ KKAwsSDK/HaHaHaManager.swift
@@ -0,0 +1,16 @@
+//
+//  HaHaHaManager.swift
+//  KKAwsSDK
+//
+//  Created by YuppTV on 10/01/19.
+//  Copyright © 2019 YuppTV. All rights reserved.
+//
+
+import Foundation
+
+public class HaHaHaManager {
+    public init() { }
+    public func awesomeFunction(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ KKAwsSDK/KKAwsSDK.h
@@ -0,0 +1,19 @@
+//
+//  KKAwsSDK.h
+//  KKAwsSDK
+//
+//  Created by YuppTV on 10/01/19.
+//  Copyright © 2019 YuppTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKAwsSDK.
+FOUNDATION_EXPORT double KKAwsSDKVersionNumber;
+
+//! Project version string for KKAwsSDK.
+FOUNDATION_EXPORT const unsigned char KKAwsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKAwsSDK/PublicHeader.h>
+
+
